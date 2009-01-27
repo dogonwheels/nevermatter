4db973be1beeb3e47fc5e9f61dde7601e777af88
Dom Crayford <domcrayford@gmail.com>
2009-01-27 21:08:55 +0000

    added initial image model to gallery

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    image_file = models.ImageField(upload_to='media')
+    title = models.CharField(max_length=200)
+    description = models.TextField()
