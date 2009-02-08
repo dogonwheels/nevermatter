bd8f0fbdb70053239f8fc0e02c88fd5e5d98daab
Dom Crayford <domcrayford@gmail.com>
2009-02-08 11:43:50 +0000

    Added 3 square reformats on save

--- gallery/models.py
+++ gallery/models.py
@@ -3,19 +3,39 @@ from django.conf import settings
 import os
 import Image
 
-# Create your models here.
 class Exhibit(models.Model):
     image_file = models.ImageField(upload_to='uploads')
     title = models.CharField(max_length=200)
     description = models.TextField()
+
+    def _prefix_path(self, prefix, full_filename):
+        (path, filename) = os.path.split(full_filename)
+        return os.path.join(path, prefix + filename)
     
-    def square_image_filename(self):
-        # This is duplicated, and subject to borkage if the filename contains '.'s!
-        return self.image_file.file.name.replace('.', '_square.')
-    
-    def square_image_url(self):
-        return self.image_file.url.replace('.', '_square.')
-    
+    @property
+    def _small_image_filename(self):
+        return self._prefix_path('small_', self.image_file.file.name)
+
+    @property
+    def _medium_image_filename(self):
+        return self._prefix_path('medium_', self.image_file.file.name)
+
+    @property
+    def _large_image_filename(self):
+        return self._prefix_path('large_', self.image_file.file.name)
+
+    @property
+    def small_image_url(self):
+        return self._prefix_path('small_', self.image_file.url)
+
+    @property
+    def medium_image_url(self):
+        return self._prefix_path('medium_', self.image_file.url)
+
+    @property
+    def large_image_url(self):
+        return self._prefix_path('large_', self.image_file.url)
+
     def save(self, force_insert=False, force_update=False):
         super(Exhibit, self).save(force_insert, force_update)
         filename = self.image_file.file.name
@@ -28,14 +48,20 @@ class Exhibit(models.Model):
             square_image = Image.new(original_image.mode, (smallest_dimension, smallest_dimension))
             square_image.paste(original_image, offset)
             
-            square_300_image = square_image.resize((300, 300), Image.ANTIALIAS)
-            square_image_filename = self.square_image_filename()
-            square_300_image.save(square_image_filename)
-        
+            large_image = square_image.resize((500, 500), Image.ANTIALIAS)
+            large_image.save(self._large_image_filename, quality=90)
+            
+            medium_image = square_image.resize((300, 300), Image.ANTIALIAS)
+            medium_image.save(self._medium_image_filename, quality=90)
+            
+            small_image = square_image.resize((100, 100), Image.ANTIALIAS)
+            small_image.save(self._small_image_filename, quality=90)
+            
     def delete(self):
-        filename = self.square_image_filename()
-        if not filename == '':
-            os.remove(filename)
+        image_files = [self._small_image_filename, self._medium_image_filename, self._large_image_filename]
+        for filename in image_files:
+            if not filename == '':
+                os.remove(filename)
         
         super(Exhibit, self).delete()
     
--- gallery/views.py
+++ gallery/views.py
@@ -9,7 +9,7 @@ def overview(request):
     return render_to_response('gallery/index.html', locals())
     
 def exhibit(request, exhibit_id):
-    main_image_url = Exhibit.objects.get(id=exhibit_id).image_file.url
+    main_image_url = Exhibit.objects.get(id=exhibit_id).large_image_url
     number_of_images = Exhibit.objects.count()
         
     return render_to_response('gallery/image.html', locals())
